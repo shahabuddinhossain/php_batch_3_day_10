462588a7ce4b5f113038fe54f1d0f917c36daa94
shahabuddinhossain <voc_1992@yahoo.com>
2022-01-30 09:54:47 +0600

    Day 10 stracture ready to start

---
+++ app/classes/Example.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\classes;
+class Example
+{
+    public $name;
+    public $email;
+    public $mobile;
+
+    public function index() {
+        echo "Hello World from Example";
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "vendor/autoload.php";
+use App\classes\Example;
+
+
+$example = new Example();
+$example->index();
